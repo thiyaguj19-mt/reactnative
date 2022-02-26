781ad648d26d66574f4d3f3d0db3af1852f30de5
Thiyagarajan Mohan <thiyaguj19@gmail.com>
2022-02-25 20:04:16 -0800

    react-native-completion-of-week1-exercise

--- nucampsite/components/MainComponent.js
+++ nucampsite/components/MainComponent.js
@@ -6,6 +6,8 @@ import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import Constants from 'expo-constants';
+import Home from './HomeComponent';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -26,7 +28,34 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+  {
+      Home: { screen: Home }
+  },
+  {
+      defaultNavigationOptions: {
+          headerStyle: {
+              backgroundColor: '#5637DD'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+              color: '#fff'
+          }
+      }
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: { screen : HomeNavigator },
+    Directory: { screen : DirectoryNavigator }
+  },
+  {
+    drawerBackgroundColor : '#CEC8FF'
+  }
+);
+
+const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
 
