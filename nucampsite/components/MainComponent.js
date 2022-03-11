bd04cb998e83752c66866148908f8ab4aea55a89
Thiyagarajan Mohan <thiyaguj19@gmail.com>
2022-03-11 11:53:09 -0800

    added my favorites link

--- nucampsite/components/MainComponent.js
+++ nucampsite/components/MainComponent.js
@@ -15,6 +15,7 @@ import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from 'react-redux';
 import { fetchComments, fetchCampsites, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoritesComponent';
 
 const mapDispatchToProps = {
     fetchComments,
@@ -144,6 +145,29 @@ const ReservationNavigator = createStackNavigator(
     }
 );
 
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView
@@ -194,6 +218,20 @@ const MainNavigator = createDrawerNavigator(
             drawerIcon: ({tintColor}) => (
                 <Icon
                     name='tree'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
+        }
+    },
+    Favorites: {
+        screen: FavoritesNavigator,
+        navigationOptions: {
+            drawerLabel: 'My Favorites',
+            drawerIcon: ({tintColor}) => (
+                <Icon
+                    name='heart'
                     type='font-awesome'
                     size={24}
                     color={tintColor}
--- nucampsite/shared/baseUrl.js
+++ nucampsite/shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://10.0.0.79:3001/';
+export const baseUrl = 'http://10.0.0.2:3001/';
